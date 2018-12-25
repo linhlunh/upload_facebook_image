9ba063db2732c7307f3062652cb5682516b95bd7
dududucanh <meoconyy123@gmail.com>
2018-12-25 18:02:12 +0700

    upload-view-moi

--- application/views/landing_page/post_photo_facebook/post_photo.php
+++ application/views/landing_page/post_photo_facebook/post_photo.php
@@ -10,7 +10,7 @@
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
     <script src="../../../../assets/libs/usage_datetime/js/mobiscroll.jquery.min.js"></script>
     <link rel="stylesheet" href="../../../../assets/libs/usage_datetime/css/mobiscroll.jquery.min.css">
-    <link rel="stylesheet" href="../../../assets/css/upload-12-25-2.css">
+    <link rel="stylesheet" href="../../../assets/css/upload-12-25-4.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
 </head>
