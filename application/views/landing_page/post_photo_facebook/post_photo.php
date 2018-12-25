74a9f8e3b08ed26f5cd499694038113575987640
dududucanh <meoconyy123@gmail.com>
2018-12-25 17:54:18 +0700

    upload view

--- application/views/landing_page/post_photo_facebook/post_photo.php
+++ application/views/landing_page/post_photo_facebook/post_photo.php
@@ -10,7 +10,7 @@
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
     <script src="../../../../assets/libs/usage_datetime/js/mobiscroll.jquery.min.js"></script>
     <link rel="stylesheet" href="../../../../assets/libs/usage_datetime/css/mobiscroll.jquery.min.css">
-    <link rel="stylesheet" href="../../../assets/css/upload-12-25-1.css">
+    <link rel="stylesheet" href="../../../assets/css/upload-12-25-2.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
 </head>
@@ -23,7 +23,7 @@
         <div class="detail-content">
 
             <div class="img-prize">
-                <img src="../../../assets/img/img-facebook/giai thuong.png" alt="">
+                <img src="../../../assets/img/img-facebook/giaithuong_nen.png" alt="">
             </div>
             <form action="" method="POST" enctype="multipart/form-data" onsubmit='return validateForm()'>
                 <div class="tquan">
