1f7a93f0f336bb6a21bef7073a3da77bb4c91b1b
linhlunh <44716024+linhlunh@users.noreply.github.com>
2018-12-26 14:57:47 +0700

    Revert "update view title page"
    This reverts commit a30a90ad045af6ddfec65e12a0b26d0c4366991d.

--- application/views/landing_page/post_photo_facebook/post_photo.php
+++ application/views/landing_page/post_photo_facebook/post_photo.php
@@ -2,12 +2,7 @@
 <html lang="en">
 
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, maximum-scale=1.0" />
-	<meta name="description" content="BestPrice - Đại lý đặt phòng khách sạn, vé máy bay, tour du lịch trực tuyến giá tốt nhất.  Khuyến mại, ưu đãi lớn - Dịch vụ tin cậy, hỗ trợ 24/7." />
-	<meta name="robots" content="noindex,nofollow" />
-	<title>Đăng ảnh đón xuân - Khuân tour miễn phí - BestPrice</title>
+
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
@@ -17,7 +12,7 @@
     <link rel="stylesheet" href="../../../../assets/libs/usage_datetime/css/mobiscroll.jquery.min.css">
     <link rel="stylesheet" href="../../../assets/css/upload-12-25-5.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-	<link rel="shortcut icon" href="https://owa.bestprice.vn/assets/img/favicon.27042017.ico">
+
 </head>
 
 <body class="body-content">
