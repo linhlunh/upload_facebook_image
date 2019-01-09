b2ca2f4bbd2a4ea1a65dfe6eea696e5a56eb6f39
dududucanh <meoconyy123@gmail.com>
2019-01-09 15:56:59 +0700

    upload-view-css

--- application/views/landing_page/post_photo_facebook/post_photo.php
+++ application/views/landing_page/post_photo_facebook/post_photo.php
@@ -19,7 +19,7 @@
     <!-- <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script> -->
     <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
     
-    <link rel="stylesheet" href="../../../assets/css/upload.min.090120191530.css">
+    <link rel="stylesheet" href="../../../assets/css/upload.min.090120191555.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="shortcut icon" href="https://owa.bestprice.vn/assets/img/favicon.27042017.ico">
     <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
